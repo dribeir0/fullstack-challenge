f8d8d7d4f3360e8d8d275ce5925477c9ac1e747c
Diogo Ribeiro <diogo.ribeiro@evooq.ch>
2023-07-11 15:09:16 +0100

    feat: add min length to password

--- cozero-backend/src/users/dto/user-login.dto.ts
+++ cozero-backend/src/users/dto/user-login.dto.ts
@@ -1,9 +1,10 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 
 export class UserLoginDto {
   @IsEmail()
   @IsNotEmpty()
   email: string;
+  @MinLength(6)
   @IsNotEmpty()
   password: string;
 }
